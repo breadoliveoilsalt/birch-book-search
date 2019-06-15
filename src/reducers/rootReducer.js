afce2eec94bf3b5740e89e5dc3239d9be1cb203b
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-06-15 15:30:25 -0400

    Add files and folders in preparation of connecting app to Redux store:
    Added to src:
    
    /enhancers/monitorReducer
    /middlware/logger
    /reducers/rootReducer
    
    And to root:
    
    configureStore.js
    
    Connection to Redux to be tested, still need to configure index.js.

---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,14 @@
+import { combineReducers } from 'redux'
+// import randomDotsReducer from './randomDotsReducer'
+// import rainReducer from './rainReducer'
+
+
+const rootReducer = combineReducers(
+  {
+    // randomDots: randomDotsReducer,
+    // rain: rainReducer,
+
+  }
+)
+
+export default rootReducer
