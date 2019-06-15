ee3bd60cb0844348c6d38ad28e403aa993f9d692
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-06-15 15:43:56 -0400

    Confirm that Redux Store is connected to app:
    Confirmed via testReducer connected to rootReducer and seeing testReducer in devTools for Redux

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
+import testReducer from './testReducer'
 // import randomDotsReducer from './randomDotsReducer'
 // import rainReducer from './rainReducer'
 
 
 const rootReducer = combineReducers(
   {
+    testReducer: testReducer
     // randomDots: randomDotsReducer,
     // rain: rainReducer,
 
