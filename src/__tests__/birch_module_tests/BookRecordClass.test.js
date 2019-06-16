f9069c034bb7021e1584e46d36c17047eaf9dd23
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-06-16 15:04:10 -0400

    Draft tests: draft plain language test for BookRecordClass

---
+++ src/__tests__/birch_module_tests/BookRecordClass.test.js
@@ -0,0 +1,26 @@
+// These are not all the relevant imports I will need.  To update.
+
+import React from 'react'
+import { expect } from 'chai'
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+
+describe("BookRecordClass", function() {
+
+  it("should throw an error if not initialized with an object", function(){
+
+  })
+
+  it("should return an instance of BookRecord", function() {
+
+  })
+
+  describe("a BookRecord instance", function() {
+    it("should have properties for title, author, publisher, and linkToExternalInfo", function() {
+
+    })
+  })
+})
