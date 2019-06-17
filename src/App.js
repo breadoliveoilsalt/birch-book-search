3d86757877ec4a50fce61877e8d4a4daed846d34
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-06-17 12:38:27 -0400

    Play with css for header and navlinks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+// up to getting NavLinks to work -- need to incorporate browserouter
 
 import Header from './components_presentational/Header'
+import NavBar from './components_presentational/NavBar'
 
 const App = () => {
 
   return (
-    <div className="app-container">
-      <Header />
-    </div>
+    <BrowserRouter>
+      <div className="app-container">
+
+        <Header />
+        <NavBar />
+      </div>
+    </ BrowserRouter>
   )
 
 }
