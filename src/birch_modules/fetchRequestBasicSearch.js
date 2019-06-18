7da37cf2fa129cb58b0eda59859a48f5804b224a
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-06-17 21:49:19 -0400

    Working on Basic Fetch: incorporate try/catch pattern into reviewing records
    Did this because I found some inconsistent records returned from google,
    and implementing try/catch pattern in each individual check
    seemed the way to go.  If anything hits catch, it returns null.
    
    To refactor to simplify code.

--- src/birch_modules/fetchRequestBasicSearch.js
+++ src/birch_modules/fetchRequestBasicSearch.js
@@ -1,9 +1,10 @@
 import fetch from 'isomorphic-fetch'
 import { deleteError, loadError } from './actionCreatorsAppStatus'
-// need to load result number
+import { loadSearchTerms, increaseSearchStartingID, loadSearchResults, resetSearch, loadResultNumber } from './actionCreatorsUpdateSearchResults'
 
-// const apiKey = process.env.REACT_APP_GOOGLE_BOOKS_API_KEY
-const apiKey =""
+
+const apiKey = process.env.REACT_APP_GOOGLE_BOOKS_API_KEY
+// const apiKey =""
 const baseURL = `https://www.googleapis.com/books/v1/volumes?key=${apiKey}`
 
 export function getBookRecords(searchTerms, searchStartingID, resultsPerSearch) {
@@ -20,24 +21,32 @@ export function getBookRecords(searchTerms, searchStartingID, resultsPerSearch) 
 
     fetch(baseURL + "&q=" + escapedSearchTerms + "&startIndex=" + searchStartingID + "&maxResults=" + resultsPerSearch)
 
-      .then(response => {
-        // checkForErrorCode(response)
-        return response.json()
-      })
+      .then(response => response.json())
+        // .then(throwable()) => having a function that just throws an error stops React - it does not get picked up by the catch statement
+
       .then(response => {
         if (response.error) {
           let message = response.error.errors[0].message + ": " + response.error.errors[0].reason
           throw new Error(message)
+        } else {
+          return response
         }
-
-        console.log("Here's the response:  ", response)
+      })
+      .then(response => {
+        dispatch(loadResultNumber(response.totalItems))
+        let bookRecords = createBookRecords(response.items) // an array
       })
       .catch(error => {
+        debugger
         dispatch(loadError(error.message))
       })
 
   }
 }
+
+// function throwable(){
+//   return new Error("From throwable")
+// }
 
 function isEmpty(searchTerms) {
   return searchTerms === "" ? true : false
@@ -48,12 +57,99 @@ function escapeSearchTerms(searchTerms) {
   return searchTerms.trim()
 }
 
-function checkForErrorCode(response) {
-  debugger
-  let statusCode = response.status
-  if (Math.floor(statusCode/100) === 4) {
-    let errorMessage = "Yo"
-    return new Error("Yo")
-  }
-  debugger
+// function checkForErrorCode(response) {
+//   debugger
+//   let statusCode = response.status
+//   if (Math.floor(statusCode/100) === 4) {
+//     let errorMessage = "Yo"
+//     return new Error("Yo")
+//   }
+//   debugger
+// }
+
+function createBookRecords(arrayOfAPIReturns) {
+
+  let bookRecordsforReduxState = []
+
+    arrayOfAPIReturns.forEach( bookRecord => {
+        console.log("Record: ", bookRecord)
+        let imageURL = getImageURL(bookRecord)
+        let title = getTitle(bookRecord)
+        let authors = getAuthors(bookRecord)
+        let publisher = getPublisher(bookRecord)
+        let additionalInfoURL = getAdditionalInfoURL(bookRecord)
+
+        debugger
+    })
+
+    function getImageURL(bookRecord) {
+      try {
+        let imageURL = null
+        if (exists(bookRecord.volumeInfo.imageLinks.thumbnail)) {
+          imageURL = bookRecord.volumeInfo.imageLinks.thumbnail
+        }
+        return imageURL
+      } catch {
+        return null
+      }
+    }
+
+    function getTitle(bookRecord) {
+      // Consider adding subtitle.
+      try {
+        let title = null
+        if (exists(bookRecord.volumeInfo.title)) {
+          title = bookRecord.volumeInfo.title
+        }
+        return title
+      } catch {
+        return null
+      }
+    }
+
+    function getAuthors(bookRecord) {
+      try {
+        let authors = null
+
+        if (exists(bookRecord.volumeInfo.authors) && bookRecord.volumeInfo.authors.length > 0) {
+          authors = bookRecord.volumeInfo.authors[0]
+
+          for (let i = 1; i < bookRecord.volumeInfo.authors.length; i++) {
+            authors += ` & ${bookRecord.volumeInfo.authors[i]}`
+          }
+        }
+        return authors
+      } catch {
+        return null
+      }
+    }
+
+    function getPublisher(bookRecord) {
+      try {
+        let publisher = null
+        if (exists(bookRecord.volumeInfo.publisher)) {
+          publisher = bookRecord.volumeInfo.publisher
+        }
+        return publisher
+      } catch {
+        return null
+      }
+    }
+
+    function getAdditionalInfoURL(bookRecord) {
+      try {
+        let additionalInfoURL = null
+        if (exists(bookRecord.volumeInfo.infoLink)) {
+          additionalInfoURL = bookRecord.volumeInfo.infoLink
+        }
+        return additionalInfoURL
+        // return exists(bookRecord.volumeInfo.infoLink) ? bookRecord.volumeInfo.infoLink : null
+      } catch {
+        return null
+      }
+    }
+}
+
+function exists(entry) {
+  return entry !== undefined && entry !== ""
 }
