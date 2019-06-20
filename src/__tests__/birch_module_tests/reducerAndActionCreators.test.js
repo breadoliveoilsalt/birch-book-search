a8f730fba756a054060dc99003f673b93cbd63a5
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-06-20 14:01:29 -0400

    Add loadError test to reducerAndActinoCreator.test.js. Working fine.

--- src/__tests__/birch_module_tests/reducerAndActionCreators.test.js
+++ src/__tests__/birch_module_tests/reducerAndActionCreators.test.js
@@ -4,24 +4,50 @@ import { expect } from 'chai'
 import { loadError, deleteError, beginBookAPIRequest, endBookAPIRequest } from '../../birch_modules/actionCreatorsAppStatus'
 
 
-describe("Initial state", function() {
+const expectedInitialState = {
+      appStatus: {
+        makingBookAPIRequest: false,
+        currentError: null
+      },
+      currentSearch:
+       { userSearchTerms: null,
+         resultsPerSearch: 20,
+         searchStartingID: 0,
+         results: [],
+         resultNumber: 0
+       }
+     }
 
-  it("does something", function() {
-    const expectedInitialState = {
-          appStatus: {
-            makingBookAPIRequest: false,
-            currentError: null
-          },
-          currentSearch:
-           { userSearchTerms: null,
-             resultsPerSearch: 20,
-             searchStartingID: 0,
-             results: [],
-             resultNumber: 0
-           }
-         }
+describe("Root reducer", function() {
+
+  it("should return the expected initial state when called without state or action arguments", function() {
 
     expect(rootReducer(undefined, {})).to.deep.equal(expectedInitialState)
+
+   })
+
+   it("should load an error message into the state when called with loadError(message)", function() {
+
+     const message = "You need more specific search terms"
+
+     const expectedState = {
+           appStatus: {
+             makingBookAPIRequest: false,
+             currentError: "You need more specific search terms"
+           },
+           currentSearch:
+            { userSearchTerms: null,
+              resultsPerSearch: 20,
+              searchStartingID: 0,
+              results: [],
+              resultNumber: 0
+            }
+          }
+
+      let newState = rootReducer(undefined, loadError(message))
+
+      expect(newState).to.deep.equal(expectedState)
+      expect(newState).to.not.equal(expectedState)
 
    })
 })
