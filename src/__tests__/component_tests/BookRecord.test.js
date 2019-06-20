1d31fbe1a4841341143ba1ab8f2284e6e13df4bf
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-06-20 17:53:03 -0400

    Flush out first test for BookRecord

--- src/__tests__/component_tests/BookRecord.test.js
+++ src/__tests__/component_tests/BookRecord.test.js
@@ -5,13 +5,41 @@ import Adapter from 'enzyme-adapter-react-16'
 
 Enzyme.configure({ adapter: new Adapter() })
 
-import BookRecord from '../components_container/BookRecord'
+import BookRecord from '../../components_presentational/BookRecord'
 
 describe("<BookRecord />", function() {
 
-  it("should render a book image if one is specified in the relevant props object", function() {
+  it("should render a book image only if imgageURL is specified in the relevant props object", function() {
 
-  })
+    const bookInfo1 = {
+      imageURL: "http://books.google.com/books/content?id=oxL9vQAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+      title: "Data Flow Diagrams - Simply Put!",
+      authors: "Thomas Hathaway & Angela Hathaway",
+      publisher: null,
+      additionalInfoURL: "http://books.google.com/books?id=oxL9vQAACAAJ&dq=DFD&hl=&source=gbs_api"
+    }
+
+    const wrapper1 = shallow(<BookRecord bookInfo={bookInfo1} />)
+
+    expect(wrapper1.exists("img")).to.be.true
+
+    /////
+
+    const bookInfo2 = {
+      imageURL: null,
+      title: "Data Flow Diagrams - Simply Put!",
+      authors: "Thomas Hathaway & Angela Hathaway",
+      publisher: null,
+      additionalInfoURL: "http://books.google.com/books?id=oxL9vQAACAAJ&dq=DFD&hl=&source=gbs_api"
+    }
+
+    const wrapper2 = shallow(<BookRecord bookInfo={bookInfo2} />)
+
+    expect(wrapper2.text()).to.include("(No image Available)")
+    expect(wrapper2.exists("img")).to.be.false
+
+
+    })
 
   it("should display a book title if one is specified in the relevant Redux object", function() {
 
--- src/components_presentational/BookRecord.js
+++ src/components_presentational/BookRecord.js
@@ -5,7 +5,7 @@ import SmallDivider from './SmallDivider'
 //       { bookInfo.imageURL ? <img src={bookInfo.imageURL} /> : null }
 const BookRecord = ({ bookInfo }) => {
 
-
+  debugger
 
   return (
 
