871929f6e14e58cf67f4b4481466eb6bcaa232d7
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-06-20 14:25:12 -0400

    Add tests for actionCreatorsAppStatus to reducerAndActionCreators.test

--- src/__tests__/birch_module_tests/reducerAndActionCreators.test.js
+++ src/__tests__/birch_module_tests/reducerAndActionCreators.test.js
@@ -22,15 +22,17 @@ describe("Root reducer", function() {
 
   it("should return the expected initial state when called without state or action arguments", function() {
 
-    expect(rootReducer(undefined, {})).to.deep.equal(expectedInitialState)
+    let newState = rootReducer(undefined, {})
+    expect(newState).to.deep.equal(expectedInitialState)
+    expect(newState).to.not.equal(expectedInitialState)
 
    })
 
-   it("should load an error message into the state when called with loadError(message)", function() {
+   it("should load an error message into the state when called with #loadError(message)", function() {
 
      const message = "You need more specific search terms"
 
-     const expectedState = {
+     const expectedNewState = {
            appStatus: {
              makingBookAPIRequest: false,
              currentError: "You need more specific search terms"
@@ -44,10 +46,78 @@ describe("Root reducer", function() {
             }
           }
 
-      let newState = rootReducer(undefined, loadError(message))
+      let newState = rootReducer(expectedInitialState, loadError(message))
 
-      expect(newState).to.deep.equal(expectedState)
-      expect(newState).to.not.equal(expectedState)
+      expect(newState).to.deep.equal(expectedNewState)
 
    })
+
+   it("should replace an error message in the statue with null when called with #deleteError(message)", function() {
+
+     const currentState = {
+           appStatus: {
+             makingBookAPIRequest: false,
+             currentError: "You need more specific search terms"
+           },
+           currentSearch:
+            { userSearchTerms: null,
+              resultsPerSearch: 20,
+              searchStartingID: 0,
+              results: [],
+              resultNumber: 0
+            }
+          }
+
+      let newState = rootReducer(currentState, deleteError())
+
+      expect(newState).to.deep.equal(expectedInitialState)
+
+   })
+
+
+   it("should indicate an API request is being made when called with #beginBookAPIRequest()", function() {
+
+      const expectedNewState = {
+            appStatus: {
+              makingBookAPIRequest: true,
+              currentError: null
+            },
+            currentSearch:
+             { userSearchTerms: null,
+               resultsPerSearch: 20,
+               searchStartingID: 0,
+               results: [],
+               resultNumber: 0
+             }
+           }
+
+      let newState = rootReducer(expectedInitialState, beginBookAPIRequest())
+
+      expect(newState).to.deep.equal(expectedNewState)
+
+   })
+
+   it("should indicate an API request has ended when called with #endBookAPIRequest()", function() {
+
+     const currentState = {
+           appStatus: {
+             makingBookAPIRequest: true,
+             currentError: null
+           },
+           currentSearch:
+            { userSearchTerms: null,
+              resultsPerSearch: 20,
+              searchStartingID: 0,
+              results: [],
+              resultNumber: 0
+            }
+          }
+
+
+      let newState = rootReducer(currentState, endBookAPIRequest())
+
+      expect(newState).to.deep.equal(expectedInitialState)
+
+   })
+
 })
