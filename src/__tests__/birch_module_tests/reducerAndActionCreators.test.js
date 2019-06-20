77c12becd5b043b8d5cf81eba3efa7246c335e43
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-06-20 15:03:45 -0400

    Add test for actionCreatorsUpdateSearchResults to reducerAndActionCreators.test

--- src/__tests__/birch_module_tests/reducerAndActionCreators.test.js
+++ src/__tests__/birch_module_tests/reducerAndActionCreators.test.js
@@ -2,6 +2,7 @@ import rootReducer from '../../reducers/rootReducer'
 
 import { expect } from 'chai'
 import { loadError, deleteError, beginBookAPIRequest, endBookAPIRequest } from '../../birch_modules/actionCreatorsAppStatus'
+import { loadSearchTerms, increaseSearchStartingID, loadSearchResults, loadResultNumber, resetSearch } from '../../birch_modules/actionCreatorsUpdateSearchResults'
 
 
 const expectedInitialState = {
@@ -22,7 +23,7 @@ describe("Root reducer", function() {
 
   it("should return the expected initial state when called without state or action arguments", function() {
 
-    let newState = rootReducer(undefined, {})
+    const newState = rootReducer(undefined, {})
     expect(newState).to.deep.equal(expectedInitialState)
     expect(newState).to.not.equal(expectedInitialState)
 
@@ -46,7 +47,7 @@ describe("Root reducer", function() {
             }
           }
 
-      let newState = rootReducer(expectedInitialState, loadError(message))
+      const newState = rootReducer(expectedInitialState, loadError(message))
 
       expect(newState).to.deep.equal(expectedNewState)
 
@@ -68,7 +69,7 @@ describe("Root reducer", function() {
             }
           }
 
-      let newState = rootReducer(currentState, deleteError())
+      const newState = rootReducer(currentState, deleteError())
 
       expect(newState).to.deep.equal(expectedInitialState)
 
@@ -91,7 +92,7 @@ describe("Root reducer", function() {
              }
            }
 
-      let newState = rootReducer(expectedInitialState, beginBookAPIRequest())
+      const newState = rootReducer(expectedInitialState, beginBookAPIRequest())
 
       expect(newState).to.deep.equal(expectedNewState)
 
@@ -114,10 +115,188 @@ describe("Root reducer", function() {
           }
 
 
-      let newState = rootReducer(currentState, endBookAPIRequest())
+      const newState = rootReducer(currentState, endBookAPIRequest())
 
       expect(newState).to.deep.equal(expectedInitialState)
 
    })
+
+   it("should load search terms into the state when called with #loadSearchTerms()", function() {
+
+     const expectedNewState = {
+           appStatus: {
+             makingBookAPIRequest: false,
+             currentError: null
+           },
+           currentSearch:
+            { userSearchTerms: "Stephen King",
+              resultsPerSearch: 20,
+              searchStartingID: 0,
+              results: [],
+              resultNumber: 0
+            }
+          }
+
+
+      const newState = rootReducer(expectedInitialState, loadSearchTerms("Stephen King"))
+
+      expect(newState).to.deep.equal(expectedNewState)
+
+   })
+
+   it("should should increase the searchStartingID by 20 when called with #increaseSearchStartingID()", function() {
+
+     const expectedNewState1 = {
+           appStatus: {
+             makingBookAPIRequest: false,
+             currentError: null
+           },
+           currentSearch:
+            { userSearchTerms: null,
+              resultsPerSearch: 20,
+              searchStartingID: 20,
+              results: [],
+              resultNumber: 0
+            }
+          }
+
+
+    const newState1 = rootReducer(expectedInitialState, increaseSearchStartingID())
+
+    expect(newState1).to.deep.equal(expectedNewState1)
+
+
+    const expectedNewState2 = {
+           appStatus: {
+             makingBookAPIRequest: false,
+             currentError: null
+           },
+           currentSearch:
+            { userSearchTerms: null,
+              resultsPerSearch: 20,
+              searchStartingID: 40,
+              results: [],
+              resultNumber: 0
+            }
+          }
+
+
+
+    const newState2 = rootReducer(newState1, increaseSearchStartingID())
+
+    expect(newState2).to.deep.equal(expectedNewState2)
+
+   })
+
+   it("should add search results into the state when called with #loadSearchResults", function() {
+
+     const searchResults1 = [{
+         "imageURL": "http://books.google.com/books/content?id=2o_mEBpjucUC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+         "title": "Jimmy the Squirrel",
+         "authors": "Amr Taher",
+         "publisher": "AuthorHouse",
+         "additionalInfoURL": "http://books.google.com/books?id=2o_mEBpjucUC&dq=jimmy&hl=&source=gbs_api"
+       },
+       {
+         "imageURL": "http://books.google.com/books/content?id=OsbuBD3mkkkC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+         "title": "Jimmy Page",
+         "authors": "George Case",
+         "publisher": "Hal Leonard Corporation",
+         "additionalInfoURL": "http://books.google.com/books?id=OsbuBD3mkkkC&dq=jimmy&hl=&source=gbs_api"
+       }]
+
+     const expectedNewState1 = {
+           appStatus: {
+             makingBookAPIRequest: false,
+             currentError: null
+           },
+           currentSearch:
+            { userSearchTerms: null,
+              resultsPerSearch: 20,
+              searchStartingID: 0,
+              results: searchResults1,
+              resultNumber: 0
+            }
+          }
+
+     const newState1 = rootReducer(expectedInitialState, loadSearchResults(searchResults1))
+
+     expect(newState1).to.deep.equal(expectedNewState1)
+
+     /////
+
+     let searchResults2 = [{
+        "imageURL": "http://books.google.com/books/content?id=d0NEDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        "title": "Why Not the Best?",
+        "authors": "Jimmy Carter",
+        "publisher": "University of Arkansas Press",
+        "additionalInfoURL": "http://books.google.com/books?id=d0NEDwAAQBAJ&dq=jimmy&hl=&source=gbs_api"
+      }]
+
+     const expectedNewState2 = {
+            appStatus: {
+              makingBookAPIRequest: false,
+              currentError: null
+            },
+            currentSearch:
+             { userSearchTerms: null,
+               resultsPerSearch: 20,
+               searchStartingID: 0,
+               results: [...searchResults1, ...searchResults2],
+               resultNumber: 0
+             }
+           }
+
+     const newState2 = rootReducer(newState1, loadSearchResults(searchResults2))
+
+     expect(newState2).to.deep.equal(expectedNewState2)
+
+   })
+
+   it("should add the search results number into the state when called with #loadResultNumber()", function() {
+
+     const expectedNewState = {
+           appStatus: {
+             makingBookAPIRequest: false,
+             currentError: null
+           },
+           currentSearch:
+            { userSearchTerms: null,
+              resultsPerSearch: 20,
+              searchStartingID: 0,
+              results: [],
+              resultNumber: 15
+            }
+          }
+
+     const newState = rootReducer(expectedInitialState, loadResultNumber(15))
+
+     expect(newState).to.deep.equal(expectedNewState)
+   })
+
+   it("should reset the state back to the expected initial state when called with #resetSearch()", function(){
+
+     const currentState = {
+           appStatus: {
+             makingBookAPIRequest: false,
+             currentError: null
+           },
+           currentSearch:
+            { userSearchTerms: "azyzx",
+              resultsPerSearch: 20,
+              searchStartingID: 0,
+              results: [{title: "Nothing"}, {author: "Nobody"}],
+              resultNumber: 2
+            }
+          }
+
+     const newState = rootReducer(currentState, resetSearch())
+
+     expect(newState).to.deep.equal(expectedInitialState)
+
+   })
+
+
+
 
 })
