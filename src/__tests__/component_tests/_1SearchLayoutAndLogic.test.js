0011a7fb57d25ed71000ef37b76e822bd910e24f
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-06-22 12:45:59 -0400

    Add a few files to functions of SearchLayoutAndLogic. Don't work unfortunately. Taking break.

---
+++ src/__tests__/component_tests/_1SearchLayoutAndLogic.test.js
@@ -0,0 +1,134 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import fetchMock from 'fetch-mock'
+import { expect } from 'chai'
+
+import sinon from 'sinon'
+
+import { SearchLayoutAndLogic } from '../../components_container/SearchLayoutAndLogic'
+
+const middlewares = [thunk]
+const mockStore = configureMockStore(middlewares)
+
+import { getBookRecords } from '../../birch_modules/fetchRequestBasicSearch'
+import { loadError, deleteError, beginBookAPIRequest, endBookAPIRequest } from '../../birch_modules/actionCreatorsAppStatus'
+import { loadSearchTerms, increaseSearchStartingID, loadSearchResults, loadResultNumber, resetSearch } from '../../birch_modules/actionCreatorsUpdateSearchResults'
+
+import Enzyme, { shallow, mount } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+Enzyme.configure({ adapter: new Adapter() })
+
+
+const state = {
+      appStatus: {
+        makingBookAPIRequest: false,
+        currentError: null
+      },
+      currentSearch:
+       { userSearchTerms: null,
+         resultsPerSearch: 20,
+         searchStartingID: 0,
+         results: [],
+         resultNumber: 0
+       }
+     }
+
+it('renders without crashing', () => {
+
+  let store = mockStore(state)
+
+
+    // lesson here is really have to keep console.logging stuff and when objects are returned, go deeper into them.
+    // let wrapperProvider = mount(<Provider store={store}> <SearchLayoutAndLogic /> </Provider>)
+
+  const wrapper = shallow(<SearchLayoutAndLogic store={store} />)
+  // let spy = sinon.spy(wrapper.instance().deleteError())
+  // wrapper.instance().deleteError()
+  let event = new Event("submit")
+  // console.log(wrapper.instance().handleClearSearch(event).bind(wrapper.instance()))
+
+  console.log(wrapper.instance().escapeSearchTerms("  hello "))
+  console.log(wrapper.instance())
+  console.log(wrapper.props())
+
+
+  (wrapper.instance().handleClearSearch(event)).bind(wrapper)
+  })
+
+
+//
+//   // lesson here is really have to keep console.logging stuff and when objects are returned, go deeper into them.
+//   // let wrapperProvider = mount(<Provider store={store}> <SearchLayoutAndLogic /> </Provider>)
+//
+//   const wrapper = shallow(<SearchLayoutAndLogic store={store} />)
+//
+//   console.log(wrapper.instance().escapeSearchTerms("  hello "))
+//
+//   let event = new Event("submit")
+//   wrapper.instance().handleClearSearch(event)
+// })
+
+/////
+  // no idea why this works now. But I still can't get to the functions. Works when import does not have curly brackets around it.
+  // const wrapper = shallow(<SearchLayoutAndLogic store={store} />)
+
+  // wrapper.instance().deleteError()
+  // console.log(wrapper.props())
+  // console.log(wrapper.props().children.props)
+  // console.log(wrapper.props().children.props.deleteError)
+  // console.log(wrapper.instance())
+  // console.log(wrapper.find(SearchLayoutAndLogic).props())
+  // console.log(wrapper.find(SearchLayoutAndLogic).render())
+  //
+  // console.log(wrapper.props().loadError)
+  // // let spy = sinon.spy(wrapper.instance())
+  //
+  // console.log(wrapper.debug())
+  //
+  // console.log(wrapper.props())
+
+
+/////////
+  // console.log("Props: ", wrapperProvider.props())
+
+  // not work:
+  // console.log(wrapperProvider.instance().find(SearchLayoutAndLogic))
+  // not vomit but not quite work
+  // console.log(wrapperProvider.instance())
+
+  // console.log("Children: ", wrapperProvider.children())
+
+  // let wrapperSearchLayoutAndLogic = wrapperProvider.find(SearchLayoutAndLogic).instance().handleClearSearch
+  // let wrapperSearchLayoutAndLogic = wrapperProvider.find(SearchLayoutAndLogic).instance().handleClearSearch
+
+// NOT WORK;
+
+  // let wrapperSearchLayoutAndLogic = wrapperProvider.find(SearchLayoutAndLogic).render().instance()
+
+  // console.log("ReactWrapper :", wrapperSearchLayoutAndLogic)
+
+  // console.log(wrapper.debug())
+  // console.log(store.getState())
+  // let mockEvent = new Event("submit")
+  // console.log(wrapper.instance()) // does something weird -- try find(SearchLayoutAndLogic)
+
+  // const div = document.createElement('div')
+  // ReactDOM.render(<Provider store={store}> <SearchLayoutAndLogic /> </Provider>, div)
+  //
+  //
+  // ReactDOM.unmountComponentAtNode(div);
+
+//
+// it('renders without crashing', () => {
+//
+//   let store = configureStore()
+//
+//   const div = document.createElement('div')
+//   ReactDOM.render(<Provider store={store}> <SearchLayoutAndLogic /> </Provider>, div)
+//
+//
+//   ReactDOM.unmountComponentAtNode(div);
+// });
--- src/__tests__/component_tests/_SearchLayoutAndLogic.test.js
+++ src/__tests__/component_tests/_SearchLayoutAndLogic.test.js
@@ -10,16 +10,58 @@ import Enzyme, { shallow, mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 Enzyme.configure({ adapter: new Adapter() })
 
+import sinon from 'sinon'
+
 
 it('renders without crashing', () => {
 
   let store = configureStore()
 
-  const div = document.createElement('div')
+  // let wrapperProvider = mount(<Provider store={store}> <SearchLayoutAndLogic /> </Provider>)
 
-  let wrapper = mount(<Provider store={store}> <SearchLayoutAndLogic /> </Provider>)
+  // no idea why this works now.
+  let wrapper = mount(<SearchLayoutAndLogic store={store} />)
 
-  console.log(wrapper.debug())
+
+  console.log(wrapper.instance())
+  console.log(wrapper.find(SearchLayoutAndLogic).props())
+  console.log(wrapper.find(SearchLayoutAndLogic).render())
+
+  console.log(wrapper.props().loadError)
+  let spy = sinon.spy(wrapper.instance())
+
+  console.log(wrapper.props().store)
+
+  console.log(wrapper.find(SearchLayoutAndLogic).render()[0])
+
+  console.log(wrapper.props())
+
+
+/////////
+  // console.log("Props: ", wrapperProvider.props())
+
+  // not work:
+  // console.log(wrapperProvider.instance().find(SearchLayoutAndLogic))
+  // not vomit but not quite work
+  // console.log(wrapperProvider.instance())
+
+  // console.log("Children: ", wrapperProvider.children())
+
+  // let wrapperSearchLayoutAndLogic = wrapperProvider.find(SearchLayoutAndLogic).instance().handleClearSearch
+  // let wrapperSearchLayoutAndLogic = wrapperProvider.find(SearchLayoutAndLogic).instance().handleClearSearch
+
+// NOT WORK;
+
+  // let wrapperSearchLayoutAndLogic = wrapperProvider.find(SearchLayoutAndLogic).render().instance()
+
+  // console.log("ReactWrapper :", wrapperSearchLayoutAndLogic)
+
+  // console.log(wrapper.debug())
+  // console.log(store.getState())
+  // let mockEvent = new Event("submit")
+  // console.log(wrapper.instance()) // does something weird -- try find(SearchLayoutAndLogic)
+
+  // const div = document.createElement('div')
   // ReactDOM.render(<Provider store={store}> <SearchLayoutAndLogic /> </Provider>, div)
   //
   //
