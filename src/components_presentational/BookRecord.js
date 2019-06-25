c9a2ad188ba5a7ba278ea630f0fb64bfde5f7c8b
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-06-25 16:35:43 -0400

    Delete SmallDivider and test - unused.

--- src/components_presentational/BookRecord.js
+++ src/components_presentational/BookRecord.js
@@ -1,10 +1,7 @@
 import React from 'react'
 
-import SmallDivider from './SmallDivider'
-
 const BookRecord = ({ bookInfo, number }) => {
 
-  // TOOK OUT       <SmallDivider /> FROM THE BOTTOM
 
   return (
 
