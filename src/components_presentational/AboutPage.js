475defdff62d56d6c5bb6fd555e2cf08f2dc7e91
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-06-25 16:47:13 -0400

    Add text and styling for AboutPage component

--- src/components_presentational/AboutPage.js
+++ src/components_presentational/AboutPage.js
@@ -3,8 +3,15 @@ import React from 'react'
 const Header = () => {
 
   return (
-    <div className="about-page">
-      This is the about page.
+    <div className="about-page bold-text">
+
+      <p>Thanks for visiting Birch, a tool to search for your next favorite book!</p>
+
+      <p>On the search page, simply type in a search term, click "Search," and browse the results.  Click "Clear Search" to start again.</p>
+
+      <p>The search functionality is thanks to the <a target="_blank" href="https://developers.google.com/books/"> Google Books API</a>.</p>
+
+      <p>Enjoy!</p>
     </div>
   )
 
