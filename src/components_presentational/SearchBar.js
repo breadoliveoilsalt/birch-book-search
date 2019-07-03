ed38539efb90dada923738c4f40a9aa788b1e71b
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-07-03 15:43:25 -0400

    Change SearchBar component back to form so user can hit enter after typing and have search work

--- src/components_presentational/SearchBar.js
+++ src/components_presentational/SearchBar.js
@@ -3,14 +3,14 @@ import React from 'react'
 const SearchBar = ({ handleSearchSubmit, loadSearchTerms, userSearchTerms }) => {
 
   return (
-    <div>
-      <input id="search-input"
-        type="text"
-        value={userSearchTerms}
-        onChange={(event) => loadSearchTerms(event.target.value)}
-      />
-      <input type="submit" onClick={handleSearchSubmit} value="Search" />
-    </div>
+      <form>
+        <input id="search-input"
+          type="text"
+          value={userSearchTerms}
+          onChange={(event) => loadSearchTerms(event.target.value)}
+        />
+        <input type="submit" onClick={handleSearchSubmit} value="Search" />
+      </ form>
   )
 
 }
