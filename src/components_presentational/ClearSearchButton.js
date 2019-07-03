08eb072e08fa7733f112206df22443229c022821
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-07-03 14:52:20 -0400

    Refactor ClearSearchButton and SearchBar to remove form tags and add onClick behavior to buttons (vs onSubmit for form)

--- src/components_presentational/ClearSearchButton.js
+++ src/components_presentational/ClearSearchButton.js
@@ -3,11 +3,7 @@ import React from 'react'
 const ClearSearchButton = ({ clearSearch }) => {
 
   return (
-    <div className="">
-      <form onSubmit={clearSearch}>
-        <input id="clear-search-button" type="submit" value="Clear Search" />
-      </form>
-    </div>
+    <input id="clear-search-button" type="submit" onClick={clearSearch} value="Clear Search" />
   )
 
 }
--- src/components_presentational/SearchBar.js
+++ src/components_presentational/SearchBar.js
@@ -3,15 +3,13 @@ import React from 'react'
 const SearchBar = ({ handleSearchSubmit, loadSearchTerms, userSearchTerms }) => {
 
   return (
-    <div className="">
-      <form onSubmit={handleSearchSubmit}>
-        <input id="search-input"
-          type="text"
-          value={userSearchTerms}
-          onChange={(event) => loadSearchTerms(event.target.value)}
-        />
-        <input type="submit" value="Search" />
-      </form>
+    <div>
+      <input id="search-input"
+        type="text"
+        value={userSearchTerms}
+        onChange={(event) => loadSearchTerms(event.target.value)}
+      />
+      <input type="submit" onClick={handleSearchSubmit} value="Search" />
     </div>
   )
 
