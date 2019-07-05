dfff2c6e43b13f9e2d46c0e15e5efdebf0c1c7b3
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-07-05 14:42:25 -0400

    Refactor tests for NavBar

--- src/__tests__/component_tests/NavBar.test.js
+++ src/__tests__/component_tests/NavBar.test.js
@@ -20,19 +20,23 @@ describe("<NavBar />", function() {
     expect(wrapper.find("div").hasClass("navbar")).to.be.true
   })
 
-  it("should render two Link components from react-router-dom", function() {
+  it("should render two Link components from react-router-dom with the className 'navlink'", function() {
     expect(wrapper.find(Link)).to.have.lengthOf(2)
+    expect(wrapper.find(Link).get(0).props.className).to.equal("navlink")
+    expect(wrapper.find(Link).get(1).props.className).to.equal("navlink")
   })
 
-  it("should render one Link with a path to the home page", function(){
-    // const wrapper = shallow(<NavBar />)
-    console.log(wrapper.find({to: "/"}).text())
-    expect(wrapper.find({to: "/"})).to.exist
+  it("should render one Link with a path to the home page that includes the text 'Book Search'", function(){
+    expect(wrapper.find({to: "/"})).to.have.lengthOf(1)
+    let textArray = wrapper.find({to: "/"}).get(0).props.children
+    expect(textArray).to.include("Book ")
+    expect(textArray).to.include(" Search")
   })
 
-  it("should render a Link with a path to the about page", function(){
-    // const wrapper = shallow(<NavBar />)
-    expect(wrapper.find({to: "/about"})).to.exist
+  it("should render a Link with a path to the about page that includes the text 'About'", function(){
+    expect(wrapper.find({to: "/about"})).to.have.lengthOf(1)
+    let textArray = wrapper.find({to: "/about"}).get(0).props.children
+    expect(textArray).to.include("About")
   })
 
 })
