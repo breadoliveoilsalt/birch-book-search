9af66d211b2c835ed5290d87408cb630c5831494
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-07-04 20:40:18 -0400

    Refactor tests for BigDivider

--- src/__tests__/component_tests/BigDivider.test.js
+++ src/__tests__/component_tests/BigDivider.test.js
@@ -7,16 +7,24 @@ Enzyme.configure({ adapter: new Adapter() })
 
 import BigDivider from '../../components_presentational/BigDivider'
 
+let wrapper
+
+beforeEach(function() {
+  wrapper = shallow(<BigDivider />)
+})
+
 describe("<BigDivider />", function() {
 
+  it("should render one div", function() {
+    expect(wrapper.find("div")).to.have.lengthOf(1)
+  })
+
   it("should have a class name 'big-divider'", function() {
-    const wrapper = shallow(<BigDivider />)
     expect(wrapper.hasClass("big-divider")).to.be.true
   })
 
   it("should have no text", function() {
-      const wrapper = shallow(<BigDivider />)
-      expect(wrapper.text()).to.equal("")
+    expect(wrapper.text()).to.equal("")
   })
 
 })
