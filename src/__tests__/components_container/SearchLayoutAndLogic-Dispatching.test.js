ce15428804673ca79a16c02b80b5f85a3cae65bf
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-07-10 17:40:55 -0400

    Refactor tests for SLAL-Dispatching

--- src/__tests__/components_container/SearchLayoutAndLogic-Dispatching.test.js
+++ src/__tests__/components_container/SearchLayoutAndLogic-Dispatching.test.js
@@ -35,118 +35,65 @@ describe("<SearchLayoutAndLogic /> - Testing that the Props Functions Call Dispa
 
   describe("When <SearchLayoutAndLogic /> is connected to the Redux store:", function() {
 
+    let wrapper
+    let store
+
+    beforeEach(function() {
+      store = mockStore(state)
+      wrapper = mount(<ConnectedSearchLayoutAndLogic store={store} />)
+    })
+
+    afterEach(function() {
+      store.clearActions()
+    })
+
     it("#this.props.loadError dispatches the #loadError action creator", function() {
-
-      let store = mockStore(state)
-
-      const wrapper = mount(<ConnectedSearchLayoutAndLogic store={store} />)
-
       const message = "Big Error"
-
       wrapper.find(SearchLayoutAndLogic).instance().props.loadError(message)
-
       let expectedAction = [actionCreatorsAppStatus.loadError(message)]
-
       expect(store.getActions()).to.deep.equal(expectedAction)
-
     })
 
     it("#this.props.deleteError dispatches the #deleteError action creator", function() {
-
-      let store = mockStore(state)
-
-      const wrapper = mount(<ConnectedSearchLayoutAndLogic store={store} />)
-
-      const message = "Big Error"
-
       wrapper.find(SearchLayoutAndLogic).instance().props.deleteError()
-
       let expectedAction = [actionCreatorsAppStatus.deleteError()]
-
       expect(store.getActions()).to.deep.equal(expectedAction)
-
     })
 
     it("#this.props.beginBookAPIRequest dispatches the #beginBookAPIRequest action creator", function() {
-
-      let store = mockStore(state)
-
-      const wrapper = mount(<ConnectedSearchLayoutAndLogic store={store} />)
-
-      const message = "Big Error"
-
       wrapper.find(SearchLayoutAndLogic).instance().props.beginBookAPIRequest()
-
       let expectedAction = [actionCreatorsAppStatus.beginBookAPIRequest()]
-
       expect(store.getActions()).to.deep.equal(expectedAction)
-
     })
 
     it("#this.props.endBookAPIRequest dispatches the #endBookAPIRequest action creator", function() {
-
-      let store = mockStore(state)
-
-      const wrapper = mount(<ConnectedSearchLayoutAndLogic store={store} />)
-
-      const message = "Big Error"
-
       wrapper.find(SearchLayoutAndLogic).instance().props.endBookAPIRequest()
-
       let expectedAction = [actionCreatorsAppStatus.endBookAPIRequest()]
-
       expect(store.getActions()).to.deep.equal(expectedAction)
-
     })
 
     it("#this.props.loadSearchTerms dispatches the #loadSearchTerms action creator", function() {
-
-      let store = mockStore(state)
-
-      const wrapper = mount(<ConnectedSearchLayoutAndLogic store={store} />)
-
-      const message = "Big Error"
-
       wrapper.find(SearchLayoutAndLogic).instance().props.loadSearchTerms()
-
       let expectedAction = [actionCreatorsUpdateSearchResults.loadSearchTerms()]
-
       expect(store.getActions()).to.deep.equal(expectedAction)
-
     })
 
     it("#this.props.increaseSearchStartingID dispatches the #increaseSearchStartingID action creator", function() {
-
-      let store = mockStore(state)
-
-      const wrapper = mount(<ConnectedSearchLayoutAndLogic store={store} />)
-
-      const message = "Big Error"
-
       wrapper.find(SearchLayoutAndLogic).instance().props.increaseSearchStartingID()
-
       let expectedAction = [actionCreatorsUpdateSearchResults.increaseSearchStartingID()]
-
       expect(store.getActions()).to.deep.equal(expectedAction)
-
     })
 
     it("#this.props.resetSearch dispatches the #resetSearch action creator", function() {
-
-      let store = mockStore(state)
-
-      const wrapper = mount(<ConnectedSearchLayoutAndLogic store={store} />)
-
-      const message = "Big Error"
-
       wrapper.find(SearchLayoutAndLogic).instance().props.resetSearch()
-
       let expectedAction = [actionCreatorsUpdateSearchResults.resetSearch()]
-
       expect(store.getActions()).to.deep.equal(expectedAction)
-
     })
 
+    it("#this.props.clearPriorSearch dispatches the #clearPriorSearch action creator", function() {
+      wrapper.find(SearchLayoutAndLogic).instance().props.clearPriorSearch()
+      let expectedAction = [actionCreatorsUpdateSearchResults.clearPriorSearch()]
+      expect(store.getActions()).to.deep.equal(expectedAction)
+    })
   })
-
 })
